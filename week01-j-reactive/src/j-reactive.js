6b49a345d724720444918698b705d74aa0475ebb
rhythm022 <rhythm22@yeah.net>
2021-11-02 21:54:34 +0800

    q3-递归处理对象套对象的属性结构

--- week01-j-reactive/src/j-reactive.js
+++ week01-j-reactive/src/j-reactive.js
@@ -1,4 +1,6 @@
 function defineReactive(obj,key,value){
+    observe(value)
+
     Object.defineProperty(obj,key,{
         get(){
             console.log('get',value)
@@ -16,13 +18,15 @@ function defineReactive(obj,key,value){
 
 
 const foo={
-    a:1
+    a:{
+        b:1
+    }
 }
-function observe(obj){//observe批量为一个对象的所有属性defineReactive
+function observe(obj){//observe以对象为单位为属性做defineReactive
     if(typeof obj !== "object" || obj === null){
         return obj
     }
     Object.keys(obj).forEach(key=>defineReactive(obj,key,obj[key]))
 }
 observe(foo)
-foo.a++
+foo.a.b

8a8dd3f13d75f69bb492ee80bcd66a2f4d33a7c9
rhythm022 <rhythm22@yeah.net>
2021-11-02 23:14:43 +0800

    w2-实现vm代理$data

--- week01-j-reactive/src/j-vue.js
+++ week01-j-reactive/src/j-vue.js
@@ -1,10 +1,26 @@
 import { observe } from './j-reactive.js'
+
+
+function proxy(vm){
+    Object.keys(vm.$data).forEach(key=>{
+        Object.defineProperty(vm,key,{
+            get(){
+                return vm.$data[key]
+            },
+            set(newValue){
+                vm.$data[key] = newValue
+            }
+        })
+    })
+}
 class Vue{
     constructor(options){
         this.$options = options
         this.$data = options.data
 
         observe(this.$data)
+
+        proxy(this)
     }
 }
 

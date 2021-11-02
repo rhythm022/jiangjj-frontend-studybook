eed0e720fbed6a433578cde6ea1676218f2969a0
rhythm022 <rhythm22@yeah.net>
2021-11-02 21:39:51 +0800

    q2-observe批量为一个对象的所有属性defineReactive

--- week01-j-reactive/src/j-reactive.js
+++ week01-j-reactive/src/j-reactive.js
@@ -15,8 +15,14 @@ function defineReactive(obj,key,value){
 }
 
 
-const foo={}
-
-defineReactive(foo,'a',1)
-
+const foo={
+    a:1
+}
+function observe(obj){//observe批量为一个对象的所有属性defineReactive
+    if(typeof obj !== "object" || obj === null){
+        return obj
+    }
+    Object.keys(obj).forEach(key=>defineReactive(obj,key,obj[key]))
+}
+observe(foo)
 foo.a++

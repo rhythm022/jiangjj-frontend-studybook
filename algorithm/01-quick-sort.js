56d9fc3049e930a9284207f080b6544700e02032
jiangjj <jiangjj@sse.net.cn>
2021-11-11 18:30:45 +0800

    update: leetcode #912

--- algorithm/01-quick-sort.js
+++ algorithm/01-quick-sort.js
@@ -1,80 +1,52 @@
-function readline(){
-    return '2,3,2,1,6,5,4'
+function quickSort(nums,l,r){
+  if(l>=r)
+    return
+  const base = nums[r]
+  let le = l
+  for(let i = l;i<r;i++){
+    if(nums[i]<base){
+      [nums[le],nums[i]] = [nums[i],nums[le]]
+      le++
+    }
   }
+  [nums[le],nums[r]] = [nums[r],nums[le]]
 
 
-const input = readline().split(',').map(Number)
+  quickSort(nums,l,le-1)
+  quickSort(nums,le+1,r)
 
-partition(input,0,input.length-1)
-
-console.log(input)
-
-function partition(input,le,ri){
-    if(le >= ri) return 
-    const base = input[ri]
-    let brok = le
-    for(let i = le;i<ri;i++){
-        if(input[i]<base){
-            [input[brok],input[i]] = [input[i],input[brok]]
-            brok++
-        }
-    }
-    [input[brok],input[ri]] = [input[ri],input[brok]]
-
-
-    partition(input,le,brok-1)
-    partition(input,brok+1,ri)
 }
-
-
-
-
-// 第一版
-/*
-partition(input,0,input.length-1)
-function partition(input,le,ri){
-    if(le >= ri) return 
-    const base = input[ri]
-    const sm = []
-    const bg = []
-    for(let i = le;i<ri;i++){
-        if(input[i]<base){
-            sm.push(input[i])
-        }else{
-            bg.push(input[i])
-        }
-    }
-    input.splice(le,ri-le+1,...[...sm,base,...bg])
-    partition(input,le,le+sm.length-1)
-    partition(input,ri-bg.length+1,ri)
-}
-*/
-
+const xxx = [10,5,4,7,6,3,2]
+quickSort(xxx, 0, xxx.length - 1)
+console.log(xxx)
 
 
 // 第三版
 /*
-function quickSort(arr, low, high) {
-    if (high <= low) return
-    let base = arr[low]
-
+function quickSort(nums,low,high){
+    if(low>=high)
+        return
     let le = low
     let ri = high
-    while (le < ri) {
-        while (le < ri && arr[ri] >= base) ri--// 找到右边小的
-        if (le < ri) arr[le++] = arr[ri]//放到左边
+    let base = nums[low]
 
-        while (le < ri && arr[le] <= base) le++;// 找到左边大的
-        if (le < ri) arr[ri--] = arr[le];//放到右边
+    // 这个过程中双指针靠拢
+    while(le !== ri){
+        while(le !== ri && nums[ri]>=base)ri--//在这步找到可以填le坑的锅
+        if(le !== ri )nums[le++] = nums[ri]//把le坑填上但留出了ri的坑，此后，看看le是没有锅可以填re的坑
+
+        while(le !== ri && nums[le]<=base)le++//le在这步找到锅了
+        if(le !== ri )nums[ri--] = nums[le]//把ri坑上
+
     }
-    arr[le] = base;// 此时le必等于ri
-    quickSort(arr, low, le - 1);
-    quickSort(arr, le + 1, high);
+    nums[le] = base
+    quickSort(nums,low,le-1)
+    quickSort(nums,le+1,high)
+
 }
 
 
-
-const xxx = [3, 2, 1]
+const xxx = [10,5,4,7,6,3,2]
 quickSort(xxx, 0, xxx.length - 1)
 console.log(xxx)
 */

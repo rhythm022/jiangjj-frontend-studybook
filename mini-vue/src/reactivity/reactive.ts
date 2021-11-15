c4078ea22f0e1e98b5012046ce3e2fe20714f607
jiangjj <jiangjj@sse.net.cn>
2021-11-15 13:27:31 +0800

    q8:reactive的happy path单测通过

--- mini-vue/src/reactivity/reactive.ts
+++ mini-vue/src/reactivity/reactive.ts
@@ -1,3 +1,16 @@
 export function reactive(raw){
-    return new Proxy(raw,{})
+    return new Proxy(raw,{
+        get(target,key){
+            const res = Reflect.get(target,key)
+
+            // TODO 收集effect
+            return res
+        },
+        set(target,key,value){
+            const res = Reflect.set(target,key,value)
+
+            // TODO 触发effect
+            return res
+        }
+    })
 }

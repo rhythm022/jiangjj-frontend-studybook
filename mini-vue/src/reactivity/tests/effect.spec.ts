8a416fed19b202f7b8038b31b1523d7380438cbb
jiangjj <jiangjj@sse.net.cn>
2021-11-15 12:23:32 +0800

    q5:建effect happy path单测

---
+++ mini-vue/src/reactivity/tests/effect.spec.ts
@@ -0,0 +1,18 @@
+describe('effect', () => {
+    it.skip('happy path',()=>{
+        const user = reactive({
+            age:10
+        })
+
+        let nextAge
+        effect(()=>{
+            nextAge = user.age + 1
+        })
+
+        expect(nextAge).toBe(11)
+
+        //update
+        user.age++
+        expect(nextAge).toBe(12)
+    })
+});

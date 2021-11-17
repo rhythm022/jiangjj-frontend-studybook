879da09a1cf6520b8ec3b87bd3f9caae41315369
jiangjj <jiangjj@sse.net.cn>
2021-11-17 13:46:37 +0800

    q25:refactor-reactiveHandler独立成模块

---
+++ mini-vue/src/reactivity/baseHandlers.ts
@@ -0,0 +1,42 @@
+import { track, trigger } from "./effect"
+
+
+
+function createGetter(isReadonly = false) {
+    return function get(obj, key) {
+        const res = Reflect.get(obj, key)
+
+        if (!isReadonly) {
+            // 收集effect
+            track(obj, key)
+        }
+
+        return res
+    }
+}
+
+function createSetter() {
+    return function set(obj, key, value) {
+        const res = Reflect.set(obj, key, value)
+
+        // 触发effect
+        trigger(obj, key)
+        return res
+    }
+}
+
+const get = createGetter()
+const set = createSetter()
+export const reactiveHandler = {
+    get,
+    set
+}
+
+const readonlyGet = createGetter(true)
+
+export const readonlyHandler = {
+    get: readonlyGet,
+    set(obj, key, value) {
+        return true
+    }
+}
--- mini-vue/src/reactivity/reactive.ts
+++ mini-vue/src/reactivity/reactive.ts
@@ -1,29 +1,4 @@
-import { track, trigger } from "./effect"
-
-
-
-function createGetter(isReadonly = false) {
-    return function get(obj, key) {
-        const res = Reflect.get(obj, key)
-
-        if (!isReadonly) {
-            // 收集effect
-            track(obj, key)
-        }
-
-        return res
-    }
-}
-
-function createSetter() {
-    return function set(obj, key, value) {
-        const res = Reflect.set(obj, key, value)
-
-        // TODO 触发effect
-        trigger(obj, key)
-        return res
-    }
-}
+import {reactiveHandler,readonlyHandler} from './baseHandlers'
 
 export function reactive(raw) {// effect为主，reactive是辅助性的提供hook
     return createActiveObject(raw, reactiveHandler)
@@ -36,15 +11,4 @@ export function readonly(raw) {// effect为主，reactive是辅助性的提供ho
 
 function createActiveObject(raw, handler){
     return new Proxy(raw, handler)
-}
-const reactiveHandler = {
-    get: createGetter(),
-    set: createSetter()
-}
-
-const readonlyHandler = {
-    get: createGetter(true),
-    set(obj, key, value) {
-        return true
-    }
 }

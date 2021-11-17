72edf4d7dbace5f89a23668dbc06722fcc6c2920
jiangjj <jiangjj@sse.net.cn>
2021-11-17 11:13:01 +0800

    q22:refactor-让createGetter负责proxy的get的生成、createSetter负责proxy的set的生成

--- mini-vue/src/reactivity/reactive.ts
+++ mini-vue/src/reactivity/reactive.ts
@@ -1,33 +1,42 @@
 import { track, trigger } from "./effect"
 
-export function reactive(raw){// effect为主，reactive是辅助性的提供hook
-    return new Proxy(raw,{
-        get(obj,key){
-            const res = Reflect.get(obj,key)
 
+
+function createGetter(isReadonly = false) {
+    return function get(obj, key) {
+        const res = Reflect.get(obj, key)
+
+        if (!isReadonly) {
             // 收集effect
-            track(obj,key)
-            return res
-        },
-        set(obj,key,value){
-            const res = Reflect.set(obj,key,value)
-
-            // TODO 触发effect
-            trigger(obj,key)
-            return res
+            track(obj, key)
         }
+
+        return res
+    }
+}
+
+function createSetter() {
+    return function set(obj, key, value) {
+        const res = Reflect.set(obj, key, value)
+
+        // TODO 触发effect
+        trigger(obj, key)
+        return res
+    }
+}
+
+export function reactive(raw) {// effect为主，reactive是辅助性的提供hook
+    return new Proxy(raw, {
+        get: createGetter(),
+        set: createSetter()
     })
 }
 
 
-export function readonly(raw){// effect为主，reactive是辅助性的提供hook
-    return new Proxy(raw,{
-        get(obj,key){
-            const res = Reflect.get(obj,key)
-
-            return res
-        },
-        set(obj,key,value){
+export function readonly(raw) {// effect为主，reactive是辅助性的提供hook
+    return new Proxy(raw, {
+        get: createGetter(true),
+        set(obj, key, value) {
             return true
         }
     })

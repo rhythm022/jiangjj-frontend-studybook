ab998df6af1ea12203c589e4ed96df95a7f34859
jiangjj <jiangjj@sse.net.cn>
2021-12-07 10:20:55 +0800

    w2:实现isProxy

--- mini-vue/src/reactivity/reactive.ts
+++ mini-vue/src/reactivity/reactive.ts
@@ -27,3 +27,6 @@ export function isReactive(target){
 export function isReadonly(target){
     return !!target[ReactiveFlags.IS_READONLY]
 }
+export function isProxy(target) {
+    return isReactive(target) || isReadonly(target)
+}
--- mini-vue/src/reactivity/tests/readonly.spec.ts
+++ mini-vue/src/reactivity/tests/readonly.spec.ts
@@ -1,4 +1,4 @@
-import { readonly,isReadonly} from '../reactive'
+import { readonly,isReadonly,isProxy} from '../reactive'
 describe('readonly',()=>{
     it('should make nested values readonly',()=>{
         const original = {foo:1}
@@ -6,6 +6,7 @@ describe('readonly',()=>{
         expect(wrapped).not.toBe(original)
         expect(wrapped.foo).toBe(1)
         expect(isReadonly(wrapped)).toBe(true)
+        expect(isProxy(wrapped)).toBe(true)
         expect(isReadonly(original)).toBe(false)
     })
 

c94e59c148f3585fcf6543c2496260ffb6d8fd5d
jiangjj <jiangjj@sse.net.cn>
2021-12-07 13:51:12 +0800

    w7:实现如果ref包装的是对象，那么要转换成响应式对象

--- mini-vue/src/reactivity/ref.ts
+++ mini-vue/src/reactivity/ref.ts
@@ -1,10 +1,15 @@
-import { hasChanged } from '../../shared';
+import { hasChanged, isObject } from '../../shared';
 import {trackEffects,triggerEffects,isTracking} from './effect'
+import { reactive } from './reactive';
 class RefImp{
     private _value: any;
     public dep// 不同于之前把dep放在effect.ts
+    private _rawValue: any;
+    
     constructor(value){
-        this._value = value
+        this._value = isObject(value) ?  reactive(value) : value
+        this._rawValue = value
+
         this.dep = new Set()
     }
 
@@ -13,8 +18,11 @@ class RefImp{
         return this._value
     }
     set value(value){
-        if(!hasChanged(value,this._value))return 
-        this._value = value
+        if(!hasChanged(value,this._rawValue))return 
+
+        this._value = isObject(value) ?  reactive(value) : value
+        this._rawValue = value   
+         
         triggerEffects(this.dep)
     }
 }

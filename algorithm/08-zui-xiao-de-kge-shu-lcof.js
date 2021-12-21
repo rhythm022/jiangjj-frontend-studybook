bed413ef6ea83b03209f738bf12f3d5c6d86fc65
jiangjj <jiangjj@sse.net.cn>
2021-12-21 09:58:11 +0800

    08-07

---
+++ algorithm/08-zui-xiao-de-kge-shu-lcof.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number[]}
+ */
+ var getLeastNumbers = function(arr, k) {
+    arr.sort((a,b)=>a-b)
+    
+    
+    return arr.slice(0,k)
+    };

ee3d77068df7fca75cd02342c3d4fb15a3973bb4
dylan <l>
2022-01-27 21:57:23 +0800

    f6:完成 export import 打包工作

--- mini-vue/examples/hello-world/App.js
+++ mini-vue/examples/hello-world/App.js
@@ -1,3 +1,6 @@
+import { h } from '../../lib/mini-vue.esm.js'
+
+
 export const App = {// App 语义：组件
     render(){
         return h("div","hello mini-vue !!" + this.msg)
--- mini-vue/src/runtime-core/index.ts
+++ mini-vue/src/runtime-core/index.ts
@@ -1,3 +1,5 @@
 // pnpm i rollup -D
 // pnpm i tslib -D
 // pnpm i @rollup/plugin-typescript -D
+export { createApp } from './createApp'
+export { h } from './h'

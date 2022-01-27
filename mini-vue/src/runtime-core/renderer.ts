171fdeaa0cbc28a78c98d4137e8d8430b8ce5f9f
dylan <l>
2022-01-27 21:07:30 +0800

    f4:实现初始化 component 主流程

--- mini-vue/src/runtime-core/renderer.ts
+++ mini-vue/src/runtime-core/renderer.ts
@@ -10,11 +10,20 @@ function patch(vnode,container){
 }
 
 function processComponent(vnode: any, container: any) {
-    mountComponent(vnode)
+    mountComponent(vnode,container)
 }
-function mountComponent(vnode: any) {
+function mountComponent(vnode: any,container) {
     const instance = createComponentInstance(vnode)
   
     setupComponent(instance)
+    setupRenderEffect(instance,container)
+}
+
+function setupRenderEffect(instance: any,container) {
+    // 返回的 subTree 又是 vnode，vnode 稍后会被 patch(vnode,container)，
+    // 如果 h 出来的 vnode 是 element 类型，则会 mountElement 
+    const subTree = instance.render() // 调用 render 相当于 开箱
+
+    patch(subTree,container)
 }
 

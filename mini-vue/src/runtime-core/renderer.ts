c0e44809abadb6fb27c201effdfac67cc2e6d13b
jiangjj <jiangjj@sse.net.cn>
2022-01-28 17:38:54 +0800

    f12:实现 ShapeFlags

--- mini-vue/src/runtime-core/renderer.ts
+++ mini-vue/src/runtime-core/renderer.ts
@@ -1,4 +1,5 @@
 import { isObject } from "../../shared/index"
+import { ShapeFlags } from "../../shared/ShapeFlags"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode,container){
@@ -7,10 +8,12 @@ export function render(vnode,container){
 }
 
 function patch(vnode,container){ // 职责：根据 vnode 更新(mount) container位置 的界面
-    if(typeof vnode.type === 'string'){
+    const { shapeFlag } = vnode
+
+    if(shapeFlag & ShapeFlags.ELEMENT){
         processElement(vnode,container)
 
-    }else if(isObject(vnode.type)){
+    }else if(shapeFlag & ShapeFlags.STATEFUL_COMPONENT){
         processComponent(vnode,container)
 
     }
@@ -43,11 +46,11 @@ function processElement(vnode: any, container: any) {
 function mountElement(vnode: any, container: any) {
     const el = vnode.el = document.createElement(vnode.type) // 这里的 vnode 是 subTree
 
-    const { children } = vnode
+    const { children,shapeFlag } = vnode
 
-    if(typeof children === 'string'){
+    if(shapeFlag & ShapeFlags.TEXT_CHILDREN){
         el.textContent = children
-    }else if(Array.isArray(children)){
+    }else if(shapeFlag & ShapeFlags.ARRAY_CHILDREN){
         mountChildren(vnode,el)
   
     }
--- mini-vue/src/runtime-core/vnode.ts
+++ mini-vue/src/runtime-core/vnode.ts
@@ -1,11 +1,31 @@
+import { ShapeFlags } from "../../shared/ShapeFlags"
+
 export function createVNode(type,props?,children?){
     const vnode = {
         type,
         props,
         children,
+        shapeFlag:getShapeFlag(type),
         el:null
+    }
+
+    if(typeof children === 'string'){
+        vnode.shapeFlag |= ShapeFlags.TEXT_CHILDREN
+        
+    }else if(Array.isArray(children)){
+        vnode.shapeFlag |= ShapeFlags.ARRAY_CHILDREN
+
     }
 
     return vnode // vnode 基本有 type props children
 
+}
+
+
+function getShapeFlag(type){
+    if(typeof type === 'string'){
+        return ShapeFlags.ELEMENT
+    }else{
+        return ShapeFlags.STATEFUL_COMPONENT
+    }
 }

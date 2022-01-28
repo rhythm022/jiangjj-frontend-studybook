7c1467db69049449dcca7acf029c34d522327e52
jiangjj <jiangjj@sse.net.cn>
2022-01-28 10:54:45 +0800

    f9: 优化 mountElement（并加注释）

--- mini-vue/src/runtime-core/renderer.ts
+++ mini-vue/src/runtime-core/renderer.ts
@@ -6,7 +6,7 @@ export function render(vnode,container){
 
 }
 
-function patch(vnode,container){
+function patch(vnode,container){ // 职责：根据 vnode 更新(mount) container位置 的界面
     if(typeof vnode.type === 'string'){
         processElement(vnode,container)
 
@@ -20,18 +20,18 @@ function processComponent(vnode: any, container: any) {
     mountComponent(vnode,container)
 }
 function mountComponent(vnode: any,container) {
-    const instance = createComponentInstance(vnode)
+    const instance = createComponentInstance(vnode)// 可以理解为 createComponentInstance 返回了一个空对象 {}
   
-    setupComponent(instance)
-    setupRenderEffect(instance,container)
+    setupComponent(instance) // 把 setup结果 和 render函数 放入 {}空对象
+    setupRenderEffect(instance,container) // mount界面
 }
 
 function setupRenderEffect(instance: any,container) {
     // 返回的 subTree 又是 vnode，vnode 稍后会被 patch(vnode,container)，
     // 如果 h 出来的 vnode 是 element 类型，则会 mountElement 
-    const subTree = instance.render() // 调用 render 相当于 开箱
+    const subTree = instance.render() // 调用 render 相当于 开箱 
 
-    patch(subTree,container)
+    patch(subTree,container)// cool!! 目前来看，组件只是给它的 vnode 提供环境，环境是 setup结果 
 }
 
 function processElement(vnode: any, container: any) {
@@ -46,9 +46,8 @@ function mountElement(vnode: any, container: any) {
     if(typeof children === 'string'){
         el.textContent = children
     }else if(Array.isArray(children)){
-        children.forEach(vnode=>{
-            patch(vnode,el)
-        })
+        mountChildren(vnode,el)
+  
     }
 
     const { props } = vnode
@@ -58,5 +57,11 @@ function mountElement(vnode: any, container: any) {
     }
 
     container.append(el)
+}
+
+function mountChildren(vnode: any, container: any) {
+    vnode.children.forEach(vnode=>{
+        patch(vnode,container)
+    })
 }
 

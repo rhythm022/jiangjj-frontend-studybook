9fb77309db9ececa817e332a659474fbd492bdb5
jiangjj <jiangjj@sse.net.cn>
2022-01-29 11:07:39 +0800

    f16: 优化修复 reactive

--- mini-vue/src/reactivity/reactive.ts
+++ mini-vue/src/reactivity/reactive.ts
@@ -1,21 +1,25 @@
+import { isObject } from '../../shared/index'
 import {reactiveHandler,readonlyHandler,shallowReadonlyHandler} from './baseHandlers'
 export const enum ReactiveFlags{
     IS_REACTIVE = '__v_isReactive',
     IS_READONLY = '__v_isReadonly'
 }
 export function reactive(raw) {// effect为主，reactive是辅助性的提供hook
-    return createActiveObject(raw, reactiveHandler)
+    return createReactiveObject(raw, reactiveHandler)
 }
 export function shallowReadonly(raw) {
-    return createActiveObject(raw, shallowReadonlyHandler)
+    return createReactiveObject(raw, shallowReadonlyHandler)
     
 }
 
 export function readonly(raw) {// effect为主，reactive是辅助性的提供hook
-    return createActiveObject(raw, readonlyHandler)
+    return createReactiveObject(raw, readonlyHandler)
 }
 
-function createActiveObject(raw, handler){
+function createReactiveObject(raw, handler){
+    if(!isObject(raw)){
+       return  console.warn(`target ${raw} 必须是一个对象`)
+    }
     return new Proxy(raw, handler)
 }
 

51b1db67cc61a71302767faf8c2e3d5d7972b301
dylan <l>
2022-02-01 18:29:04 +0800

    f31: 利用原型链实现provide

--- mini-vue/src/runtime-core/apiInject.ts
+++ mini-vue/src/runtime-core/apiInject.ts
@@ -3,7 +3,12 @@ import { getCurrentInstance } from "./component";
 export function provide(key,value){
     const cmpIns:any = getCurrentInstance()
     if(cmpIns){
-        const {provides} = cmpIns
+        let {provides} = cmpIns
+        const parentProvides = cmpIns.parent ? cmpIns.parent.provides : null
+
+        if(provides === parentProvides){
+            provides = cmpIns.provides = Object.create(parentProvides) // cool!!
+        }
 
         provides[key] = value
     }
